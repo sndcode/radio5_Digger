339fcb1667ac75e2783f48bcadc505ac49bfdcef
Dmytro Horoshko <electric.molfar@gmail.com>
2023-12-18 15:27:08 +0200

    enable simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,32 @@
 # frozen_string_literal: true
 
+require "simplecov"
+
+SimpleCov.start do
+  enable_coverage :branch
+
+  add_filter "/spec/"
+end
+
+require "simplecov-lcov"
+
+SimpleCov::Formatter::LcovFormatter.config do |config|
+  config.report_with_single_file = true
+  config.single_report_path = "coverage/lcov.info"
+end
+
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::LcovFormatter
+])
+
 require "radio5"
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
+  # enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
+  # disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
