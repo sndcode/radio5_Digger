0af9d7e5776504162ba4332718fd0ce461950542
Dmytro Horoshko <electric.molfar@gmail.com>
2024-02-09 12:42:52 +0200

    refactor: reorganize validations

--- lib/radio5/validations.rb
+++ lib/radio5/validations.rb
@@ -1,32 +1,11 @@
 # frozen_string_literal: true
 
 module Radio5
-  module Validator
+  module Validations
     module_function
 
-    def mongo_id?(object)
-      object.is_a?(String) && object.match?(Regexps::MONGO_ID)
-    end
-
-    def country_iso_code?(object)
-      object.is_a?(String) && object.match?(Regexps::COUNTRY_ISO_CODE)
-    end
-
-    def decade?(object)
-      object.is_a?(Integer) && DECADES.include?(object)
-    end
-
-    def mood?(object)
-      object.is_a?(Symbol) && MOODS_MAPPING.key?(object)
-    end
-
-    def user_track_status?(object)
-      object.is_a?(Symbol) && USER_TRACK_STATUSES_MAPPING.key?(object)
-    end
-
-    def positive_number?(object)
-      object.is_a?(Integer) && object.positive?
-    end
+    include Matchers
+    extend Matchers
 
     def validate!
       yield
